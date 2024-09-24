42c55c04ed01c2655eea681d6b328cdd9b21af14
rodrigoo.fernandezz <rodrigoo1.fernandezz@gmail.com>
2024-09-24 18:45:15 -0300

    añadiendo un proyecto que hice de demostracion

--- Arkanoid/script.js
+++ Arkanoid/script.js
@@ -1,12 +1,43 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d'); /* aca podes utilizar (webgl, bitmaprenderer) */
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-canvas.width = 448
-canvas.height = 400
- 
-function draw () {
-    console.log ('hola')
-    window.requestAnimationFrame(draw)
+canvas.width = 448;
+canvas.height = 400;
+
+//variables del juego
+let counter = 0;
+
+// variables de la pelota
+const ballRadius = 4; //tamaño de la pelota
+
+/* saber su pocision (lo hacemos con LET porque va a ir cambiando de valor.)*/
+let x = canvas.width / 2
+let y = canvas.height - 30
+// velocidad de la pelota
+let dx = 2
+let dy = -2
+
+function drawball() {
+    
 }
+function drawpaddle() {}
+function drawbricks() {} 
 
-draw()
+function collisionDetection() {}
+function ballMovement() {}
+function paddleMovement() {}
+
+function draw() {
+  // dibujando lo pricipal
+  drawball(); //pelota
+  drawpaddle(); //paddle la barrita para la pelota
+  drawbricks(); //luego los bricks
+
+  //movimientos y coliciones
+  collisionDetection() //detecta si hubo una colision y modifica el estado
+  ballMovement() //el movimiento de la pelota
+  paddleMovement() //el movimiento del paddle
+
+  window.requestAnimationFrame(draw);
+}
+draw();
